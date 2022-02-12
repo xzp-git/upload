d2708a3a9f2c86dc44654d14e7698acf7b473860
xzp <xingzepeng@geo-compass.com>
2022-02-12 17:27:57 +0800

    初始化上传组件

--- client/src/Upload.tsx
+++ client/src/Upload.tsx
@@ -1,11 +1,22 @@
 import { Row, Col, Input } from 'antd'
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 
 
 function Upload() {
+  let [currentFile, setCurrentFile] = useState<File>()
+  let [objectURL, setObjectURL] = useState<string>('')
 
+  useEffect(() => {
+    if (currentFile) {
+      let objectURL = window.URL.createObjectURL(currentFile)
+      setObjectURL(objectURL)
+      return () => window.URL.revokeObjectURL(objectURL)
+    }
+  }, [currentFile])
   function handleChange(event:ChangeEvent<HTMLInputElement>) {
-    
+     let file:File = event.target.files![0]
+     console.log(file);
+     setCurrentFile(file)
   }
 
   return(
@@ -13,7 +24,9 @@ function Upload() {
       <Col span={12}>
         <Input type='file' style={{width:300}} onChange={handleChange}></Input>
       </Col>
-      <Col span={12}></Col>
+      <Col span={12}>
+        {objectURL && <img src={objectURL} style={{width:100}} />}
+      </Col>
     </Row>
   )
 }
